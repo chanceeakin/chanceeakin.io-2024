ae8158b3ad33e62fe2f8999e7fded959ffef4a76
Chance Eakin <chance.eakin@gmail.com>
2023-12-25 09:59:49 -0600

    changes lazy home loading behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { HomeContent } from "@/components/content/home";
+import { HomeFallback } from "@/components/suspense/home-fallback";
+import { Suspense, lazy } from "react";
+
+const HomeContent = lazy(() => import("@/components/content/home"));
 export default function Home() {
   return (
     <div className="absolute top-0 left-0 h-screen w-screen">
       <div className="h-full w-full grid text-white">
-        <HomeContent />
+        <Suspense fallback={<HomeFallback />}>
+          <HomeContent />
+        </Suspense>
       </div>
     </div>
   );
--- src/components/content/home.tsx
+++ src/components/content/home.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Link from "next/link";
-import { useState, useCallback, useRef, Suspense } from "react";
+import { useState, useCallback, useRef, Suspense, lazy } from "react";
 import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
 import { FiArrowDown } from "react-icons/fi";
 import { Boop } from "@/components/animations/boop";
 import { HomeFallback } from "../suspense/home-fallback";
 
-export const HomeContent = async () => {
+const HomeContent = () => {
   const parallax = useRef<IParallax>(null);
   const [scrollPosition, setPosition] = useState(0);
 
@@ -24,89 +24,87 @@ export const HomeContent = async () => {
     }
   }, [scrollPosition, updateScrollPosition]);
   return (
-    <Suspense fallback={<HomeFallback />}>
-      <Parallax pages={5} ref={parallax} className="!touch-auto">
-        <ParallaxLayer
-          offset={0}
-          speed={0.5}
-          className="flex justify-center flex-col"
-        >
-          <p
-            className="place-self-center text-5xl
+    <Parallax pages={5} ref={parallax} className="!touch-auto">
+      <ParallaxLayer
+        offset={0}
+        speed={0.5}
+        className="flex justify-center flex-col"
+      >
+        <p
+          className="place-self-center text-5xl
       bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 block text-transparent bg-clip-text background-animate"
-          >
-            Hi, I&apos;m
-          </p>
-          <h1
-            className="place-self-center text-center text-6xl lg:text-9xl
+        >
+          Hi, I&apos;m
+        </p>
+        <h1
+          className="place-self-center text-center text-6xl lg:text-9xl
       bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text background-animate"
-          >
-            Chance Eakin
-          </h1>
-          <div className="place-self-center">
-            <Boop>
-              <button aria-label="scroll-down-page" onClick={scroll}>
-                <svg width="0" height="0">
-                  <linearGradient
-                    id="blue-gradient"
-                    x1="100%"
-                    y1="100%"
-                    x2="0%"
-                    y2="0%"
-                  >
-                    <stop stopColor="#7a6ded" offset="0%" />
-                    <stop stopColor="#591885" offset="100%" />
-                  </linearGradient>
-                </svg>
-                <FiArrowDown
-                  size="3em"
-                  style={{
-                    stroke: "url(#blue-gradient)",
-                  }}
-                />
-              </button>
-            </Boop>
-          </div>
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={1}
-          className="flex justify-center"
-          onClick={scroll}
         >
-          {/* <ParallaxLayer sticky={{ start: 1, end: 3 }}> */}
-          <h2
-            className="place-self-center text-5xl
+          Chance Eakin
+        </h1>
+        <div className="place-self-center">
+          <Boop>
+            <button aria-label="scroll-down-page" onClick={scroll}>
+              <svg width="0" height="0">
+                <linearGradient
+                  id="blue-gradient"
+                  x1="100%"
+                  y1="100%"
+                  x2="0%"
+                  y2="0%"
+                >
+                  <stop stopColor="#7a6ded" offset="0%" />
+                  <stop stopColor="#591885" offset="100%" />
+                </linearGradient>
+              </svg>
+              <FiArrowDown
+                size="3em"
+                style={{
+                  stroke: "url(#blue-gradient)",
+                }}
+              />
+            </button>
+          </Boop>
+        </div>
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={1}
+        className="flex justify-center"
+        onClick={scroll}
+      >
+        {/* <ParallaxLayer sticky={{ start: 1, end: 3 }}> */}
+        <h2
+          className="place-self-center text-5xl
       bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text"
-          >
-            I write software,
-          </h2>
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={2}
-          speed={1.5}
-          className="flex justify-center"
-          onClick={scroll}
         >
-          <p className={PARAGRAPH_CLASSES}>live in Austin,</p>
-        </ParallaxLayer>
+          I write software,
+        </h2>
+      </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={3}
-          speed={1.5}
-          className="flex justify-center"
-          onClick={scroll}
-        >
-          <p className={PARAGRAPH_CLASSES}>and love building and learning.</p>
-        </ParallaxLayer>
-        <ParallaxLayer offset={4} speed={1.5} className="flex justify-center">
-          <h3 className={ABOUT_LINK_CLASSES}>
-            <Link href="/about">Find out more.</Link>
-          </h3>
-        </ParallaxLayer>
-      </Parallax>
-    </Suspense>
+      <ParallaxLayer
+        offset={2}
+        speed={1.5}
+        className="flex justify-center"
+        onClick={scroll}
+      >
+        <p className={PARAGRAPH_CLASSES}>live in Austin,</p>
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={3}
+        speed={1.5}
+        className="flex justify-center"
+        onClick={scroll}
+      >
+        <p className={PARAGRAPH_CLASSES}>and love building and learning.</p>
+      </ParallaxLayer>
+      <ParallaxLayer offset={4} speed={1.5} className="flex justify-center">
+        <h3 className={ABOUT_LINK_CLASSES}>
+          <Link href="/about">Find out more.</Link>
+        </h3>
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
@@ -115,3 +113,5 @@ const PARAGRAPH_CLASSES = `place-self-center text-5xl text-center
 
 const ABOUT_LINK_CLASSES = `place-self-center text-5xl
       bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text hover:from-pink-500 hover:to-yellow-500`;
+
+export default HomeContent;
