b2be09cc4a87805f230cba79180959c9ce83dc3e
Chance Eakin <chance.eakin@gmail.com>
2024-01-02 23:43:35 -0600

    fix sphere. more blog writing

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -4,7 +4,7 @@ export default function BlogLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="text-slate-50 container m-auto grid grid-cols-12 gap-4 pt-16">
+    <div className="h-full w-full text-slate-50 container m-auto grid grid-cols-12 gap-4 pt-16 overflow-auto">
       <div className="flex flex-col col-span-10 col-start-2 md:col-span-8 md:col-start-3 lg:col-start-4 lg:col-span-6">
         {children}
       </div>
--- src/components/mdx/custom-mdx-components.tsx
+++ src/components/mdx/custom-mdx-components.tsx
@@ -17,29 +17,44 @@ export const mdxComponents: MDXComponents = {
     </h1>
   ),
   h2: ({ children, ...props }) => (
-    <h2 className="text-3xl" {...props}>
+    <h2 className="text-3xl mb-10" {...props}>
       {children}
     </h2>
   ),
   h3: ({ children, ...props }) => (
-    <h3 className="text-2xl" {...props}>
+    <h3 className="text-2xl mb-10" {...props}>
       {children}
     </h3>
   ),
   h4: ({ children, ...props }) => (
-    <h4 className="text-xl" {...props}>
+    <h4 className="text-xl mb-8" {...props}>
       {children}
     </h4>
   ),
   h5: ({ children, ...props }) => (
-    <h5 className="text-lg" {...props}>
+    <h5 className="text-lg mb-6" {...props}>
       {children}
     </h5>
   ),
   h6: ({ children, ...props }) => (
-    <h6 className="text-md" {...props}>
+    <h6 className="text-md mb-4" {...props}>
       {children}
     </h6>
+  ),
+  p: ({ children, ...props }) => (
+    <p className="mb-4" {...props}>
+      {children}
+    </p>
+  ),
+  ul: ({ children, ...props }) => (
+    <ul className="mb-4" {...props}>
+      {children}
+    </ul>
+  ),
+  li: ({ children, ...props }) => (
+    <li className="list-item list-disc last:mb-4" {...props}>
+      {children}
+    </li>
   ),
   pre: Code,
 };
