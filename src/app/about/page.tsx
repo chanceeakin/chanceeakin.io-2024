ff6fa1f40ee1c8503500f46736b12f64ae9a5e77
Chance Eakin <chance.eakin@gmail.com>
2024-03-28 07:33:17 -0500

    z index fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import { AboutContent } from "@/components/content/about";
 
 export default function About() {
   return (
-    <div className="absolute top-0 left-0 h-screen w-screen">
+    <div className="z-20 absolute top-0 left-0 h-screen w-screen">
       <div className="h-full w-full grid">
         <AboutContent />
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Suspense, lazy } from "react";
 const HomeContent = lazy(() => import("@/components/content/home"));
 export default function Home() {
   return (
-    <div className="absolute top-0 left-0 h-screen w-screen">
+    <div className="z-20 absolute top-0 left-0 h-screen w-screen">
       <div className="h-full w-full grid text-white">
         <Suspense fallback={<HomeFallback />}>
           <HomeContent />
